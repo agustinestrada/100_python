2fbf39ebe59a4b1e9071f75db120cacd1e44dfda
agustinestrada <agustindeestrada91@gmail.com>
2025-10-10 09:42:58 -0300

    day21-1

---
+++ 20/main_snake.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+import time
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor('white')
+screen.title('The Snake game')
+screen.tracer(0)
+ 
+
+starting_positions = [(0,0),(-20,0),(-40,0)]
+
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle(shape='square')
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+    screen.update()
+
+
+game_is_on = True
+
+while game_is_on:
+        screen.update()
+        time.sleep(0.3)
+        for seg in segments:
+            seg.forward(20)
+
+screen.exitonclick()
