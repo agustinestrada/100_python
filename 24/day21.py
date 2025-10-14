faa66757b582ef35e12a977cdf9b6893a710bbb1
agustinestrada <agustindeestrada91@gmail.com>
2025-10-14 10:47:28 -0300

    day22

---
+++ 24/day21.py
@@ -0,0 +1,25 @@
+# Inheritance
+
+class Animal:
+
+    def __init__(self):
+        self.eyes = 2
+
+    def breath(self):
+        print('Inhale, Exhale')
+
+class Fish(Animal):
+
+    def __init__(self):
+        super().__init__()
+
+    def breath(self):
+        super().breath()
+        print('Doing this under water.')
+
+    def swim(self):
+        print('Moving in water')
+
+nemo = Fish()
+nemo.swim()
+nemo.breath()
